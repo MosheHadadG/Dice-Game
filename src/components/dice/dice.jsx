38b09bed8e154ccaf0b3c7f7c09e0a02c9862a55
MosheHadadG <Moshehadad500@gmail.com>
2022-05-19 22:49:36 +0300

    Adding alt prop to img

--- src/components/dice/dice.jsx
+++ src/components/dice/dice.jsx
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import './dice.css'
 
 class Dice extends Component {
-  
+
   render() {
     if (this.props.die1 || this.props.die2) {
       return (
         <div className="dice-box">
           <div className="dice-img">
-            <img src={require(`../images/dice/dice-${this.props.die1}.png`)}></img>
+            <img src={require(`../images/dice/dice-${this.props.die1}.png`)} alt="dice img"></img>
           </div>
           <div className="dice-img">
-            <img src={require(`../images/dice/dice-${this.props.die2}.png`)}></img>
+            <img src={require(`../images/dice/dice-${this.props.die2}.png`)} alt="dice img"></img>
           </div>
         </div>
       );
